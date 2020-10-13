5748516ca26795055dd7f506d1b549d3eb339fac
keyin94 <e0639667@u.nus.edu>
2020-10-13 16:21:29 +0800

    fix cart bugs
    resolve cart bugs

--- ASP_CA/ASP_CA/wwwroot/js/shared/index.js
+++ ASP_CA/ASP_CA/wwwroot/js/shared/index.js
@@ -10,24 +10,49 @@ function onButton(event) {
     let elem = event.currentTarget;
     let productId = elem.getAttribute("product_id");
     let product_quantity = elem.parentElement.parentElement.querySelector(".quantitylisted").getAttribute("product_quantity");
-    let quantity = elem.parentElement.querySelector(".quantitylisted").getAttribute("value");
+    let quantity = parseInt(product_quantity);
+    let product_price = elem.parentElement.parentElement.parentElement.querySelector(".quantitytotal").getAttribute("product_total");
+    let totalprice = parseInt(product_price);
+    let unitprice = totalprice / quantity;
+
+    let totalproductprice = elem.parentElement.parentElement.parentElement.parentElement.querySelector(".quantitytotalprice").getAttribute("product_totalprice");
 
     if (elem.getAttribute("name") === "plus_button") {
         let action = "/Cart/PlusOne";
-        let total = parseInt(product_quantity) + 1;
+        let total = quantity + 1;
+        let price = unitprice * total;
         elem.parentElement.querySelector(".quantitylisted").setAttribute("value", total);
-        elem.parentElement.querySelector(".quantitylisted").setAttribute("product_quantity",total);
+        elem.parentElement.querySelector(".quantitylisted").setAttribute("product_quantity", total);
+
+        elem.parentElement.parentElement.parentElement.querySelector(".quantitytotal").setAttribute("product_total", price);
+        elem.parentElement.parentElement.parentElement.querySelector(".quantitytotal").setAttribute("value", price);
+
+        let grandtotal = parseInt(totalproductprice) + unitprice; 
+
+        elem.parentElement.parentElement.parentElement.parentElement.querySelector(".quantitytotalprice").setAttribute("product_totalprice", grandtotal);
+        elem.parentElement.parentElement.parentElement.parentElement.querySelector(".quantitytotalprice").setAttribute("value",grandtotal);
+
         sendAction(action, productId);
     }
     else if (elem.getAttribute("name") === "minus_button") {
         let action = "/Cart/MinusOne";
         let total = parseInt(product_quantity) - 1;
+        let price = unitprice * total;
+
+
+        let grandtotal = parseInt(totalproductprice) - unitprice;
+
+
 
         if (total < 1)
             alert('Select remove if you want to delete the item');
         else {
             elem.parentElement.querySelector(".quantitylisted").setAttribute("value", total);
             elem.parentElement.querySelector(".quantitylisted").setAttribute("product_quantity", total);
+            elem.parentElement.parentElement.parentElement.querySelector(".quantitytotal").setAttribute("product_total", price);
+            elem.parentElement.parentElement.parentElement.querySelector(".quantitytotal").setAttribute("value", price);
+            elem.parentElement.parentElement.parentElement.parentElement.querySelector(".quantitytotalprice").setAttribute("product_totalprice", grandtotal);
+            elem.parentElement.parentElement.parentElement.parentElement.querySelector(".quantitytotalprice").setAttribute("value", grandtotal);
             sendAction(action, productId);
         }
         
